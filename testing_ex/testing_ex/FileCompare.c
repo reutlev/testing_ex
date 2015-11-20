215a911b38d6d4c03e7e457a58bd22de9ca7d0a5
Adi Mashiah <adi_mashiah@live.com>
2015-11-20 16:14:59 +0200

    adding compare module

--- testing_ex/testing_ex/FileCompare.c
+++ testing_ex/testing_ex/FileCompare.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-bool CompareOutputVersusExpected(const char *output_file, const char *expected_file, TestObject *test)
+BOOL CompareOutputVersusExpected(const char *output_file, const char *expected_file, TestObject *test)
 {
 	errno_t error_output = -1;
 	errno_t error_expected = -1;
@@ -17,21 +17,30 @@ bool CompareOutputVersusExpected(const char *output_file, const char *expected_f
 	if ( error_output != 0)
 	{
 			LOG_ERROR("Could not open an output file for compare");
-			return 1;
+			return TRUE;
 	}
 
+	error_expected = fopen_s(&expected_file_ptr, expected_file, "r");
 	if ( error_expected != 0)
 	{
 			LOG_ERROR("Could not open an expected output file for compare");
-			return 1;
+			return TRUE;
 	}
 
 	do
 	{
 		i = fgetc(output_file_ptr);
 		j = fgetc(expected_file_ptr);
-		if( i == j)
+		
+		if( i == j && feof(expected_file_ptr) && feof(expected_file_ptr))
 		{
+			//then this is the last characther in the files
+			break;
+		}
+
+		if( i == j && (!feof(expected_file_ptr)) && (!feof(output_file_ptr)))
+		{
+			//current characters are equivalent
 			continue;
 		}
 		
@@ -40,18 +49,15 @@ bool CompareOutputVersusExpected(const char *output_file, const char *expected_f
 			(feof(expected_file_ptr) && (!feof(output_file_ptr))))
 		{
 			//then they are not equal
-			printf("NOT EQUAL");
+			printf("Found unequal output file and expected outputfile.");
 			test_result = FAILED;
 			test->test_result = test_result;
-			return 0;
+			return FALSE;
 		}
 
-		test_result = SUCCEEDED;
-		test->test_result = test_result;
-		return 0;
+	}while(1);
 
-	}while(1)
-
-
-
+	test_result = SUCCEEDED;
+	test->test_result = test_result;
+	return FALSE;
 }
