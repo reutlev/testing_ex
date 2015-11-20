4079457291d212907cbee396fffbe7932f4561cf
Adi Mashiah <adi_mashiah@live.com>
2015-11-20 16:15:14 +0200

    adding compare module

--- testing_ex/testing_ex/FileCompare.h
+++ testing_ex/testing_ex/FileCompare.h
@@ -7,9 +7,9 @@
 //This function receives strings of the output file name and the expected file name, and a pointer to the TestObject
 //Goes through those files and checks whether they are equal or not. Updates the test_result files in TestObject
 //returns 0 if succeeded, 1 if failed.
-bool CompareOutputVersusExpected(
-	char output_file,
-	char expected_file
+BOOL CompareOutputVersusExpected(
+	const char *output_file,
+	const char *expected_file,
 	TestObject *test
 );
 
